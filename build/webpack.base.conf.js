d9fc7906af45962163b9138d2d4abda43b5dd369
nikita <mr.sokolov99@gmail.com>
2020-01-22 11:53:03 +0400

    Первая версия

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,120 @@
+const webpack = require('webpack')
+const path = require('path')
+const fs = require('fs')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+}
+
+const PAGES_DIR = `${PATHS.src}/pages`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    kit: `${PATHS.src}/entry/kit.js`
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].js`,
+    path: PATHS.dist,
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.pug$/,
+      oneOf: [
+        {
+          use: ['pug-loader']
+        }
+      ]
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file-loader',
+      options: {
+        outputPath: `${PATHS.assets}fonts`, 
+        name: '[name].[ext]'
+      }
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'file-loader',
+      options: {
+        outputPath: `${PATHS.assets}img`,
+        name: '[name].[ext]'
+      }
+    },  {
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { 
+            sourceMap: true,
+            url: false
+          }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+        }, {
+          loader: 'less-loader',
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { 
+            sourceMap: true,
+            url: false
+          }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+        }
+      ]
+    }]
+  },
+  plugins: [
+
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      '$': 'jquery',
+      jquery: 'jquery',
+      jQuery: 'jquery',
+      'window.jquery': 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].css`,
+    }),
+
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+      { from: `${PATHS.src}/static`, to: 'static' },
+    ]),
+
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      inject: false,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    }))
+  ],
+}
+
