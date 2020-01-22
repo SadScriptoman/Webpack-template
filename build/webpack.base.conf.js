3c7d40a98013f67a59f1809add7a863f2b604a3e
nikita <mr.sokolov99@gmail.com>
2020-01-22 12:04:39 +0400

    Обновил base config чтобы html копировался из pages

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,7 +12,8 @@ const PATHS = {
 }
 
 const PAGES_DIR = `${PATHS.src}/pages`
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+const PAGES_PUG = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+const PAGES_HTML = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.html'))
 
 module.exports = {
   externals: {
@@ -52,7 +53,26 @@ module.exports = {
         outputPath: `${PATHS.assets}img`,
         name: '[name].[ext]'
       }
-    },  {
+    }, {
+      test: /\.s[ac]ss$/i,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { 
+            sourceMap: true,
+            url: false
+          }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+        }, {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        }
+      ],
+    }, {
       test: /\.less$/,
       use: [
         'style-loader',
@@ -110,10 +130,16 @@ module.exports = {
       { from: `${PATHS.src}/static`, to: 'static' },
     ]),
 
-    ...PAGES.map(page => new HtmlWebpackPlugin({
+    ...PAGES_PUG.map(page => new HtmlWebpackPlugin({
       template: `${PAGES_DIR}/${page}`,
       inject: false,
       filename: `./${page.replace(/\.pug/,'.html')}`
+    })),
+    
+    ...PAGES_HTML.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      inject: false,
+      filename: `./${page}`
     }))
   ],
 }
